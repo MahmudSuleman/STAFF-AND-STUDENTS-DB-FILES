d67cba9c141ba0b72d7fc9f9e793994c7b346f17
ZakariaRahim <37776237+ZakariaRahim@users.noreply.github.com>
2019-07-16 01:29:00 -1200

    Add files via upload

---
+++ StaffInfor.sql
@@ -0,0 +1,10 @@
+StaffId varchar (11) primary key
+StaffPassword varchar (30)
+FullName varchar (100)
+TelNumber varchar(100)
+Region varchar (100)
+City varchar (100)
+CourseTeaching varchar (100)
+DOB date
+DateAdmitted date
+Hobby varchar (100)
