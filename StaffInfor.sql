8a657cae90b225e5da1be8aab0a6fc09edd74544
Mahmud Chamtooni <38923023+MahmudSuleman@users.noreply.github.com>
2019-07-28 14:45:48 +0000

    Update StaffInfor.sql
    Just transformed it into an SQL statement..

--- StaffInfor.sql
+++ StaffInfor.sql
@@ -1,10 +1,11 @@
-StaffId varchar (11) primary key
-StaffPassword varchar (30)
-FullName varchar (100)
-TelNumber varchar(100)
-Region varchar (100)
-City varchar (100)
-CourseTeaching varchar (100)
-DOB date
-DateAdmitted date
-Hobby varchar (100)
+CREATE TABLE staffInfo(
+StaffId varchar (11) primary key,
+StaffPassword varchar (30) not null,
+FullName varchar (100) not null,
+TelNumber varchar(100),
+Region varchar (100),
+City varchar (100),
+CourseTeaching varchar (100) not null,
+DOB date not null,
+DateAdmitted date not null,
+Hobby varchar (100));
